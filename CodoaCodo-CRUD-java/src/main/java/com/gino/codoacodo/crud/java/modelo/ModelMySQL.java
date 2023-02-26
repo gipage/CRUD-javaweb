3201112429b55d385de7b24e80a754959abf454d
Gino Paoletti Geuna <ginopaolettigeuna30@gmail.com>
2023-02-26 00:07:48 -0300

    dependecias

---
+++ CodoaCodo-CRUD-java/src/main/java/com/gino/codoacodo/crud/java/modelo/ModelMySQL.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gino.codoacodo.crud.java.modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author ginop
+ */
+public class ModelMySQL implements Modelo {
+
+    @Override
+    public List<Jugador> getJugadores() {
+        return null;
+    }
+
+    @Override
+    public Jugador getJugador(int id) {
+        return null;
+    }
+
+    @Override
+    public int addJugador(Jugador jugador) {
+        return 0;
+    }
+
+    @Override
+    public int updateJugador(Jugador jugador) {
+        return 0;
+    }
+
+    @Override
+    public int removeJugador(int id) {
+        return 0;
+    }
+    
+}
